aeb4acd6d4b15fe2f4c3c8b5c4f754a58055066b
JDW <xs@jdw.sx>
2022-07-10 15:04:52 -0400

    Add Phan config.

---
+++ .phan/config.php
@@ -0,0 +1,20 @@
+<?php
+
+declare( strict_types = 1 );
+
+
+return [
+
+	'target_php_version' => '8.1',
+
+	'directory_list' => [
+		'src',
+		'tests',
+		'vendor',
+	],
+
+	'exclude_analysis_directory_list' => [
+		'vendor'
+	],
+];
+
