1d2bf045775a5f267822dd2a95aabae4291e78b3
JDW <xs@jdw.sx>
2022-07-18 21:30:27 -0400

    Add the NamedRoot class for bootstrapping.
    This class reads the root zone file as published by IANA.  It
    provides a useful (and hopefully easy-to-maintain) bootstrap for
    finding the global root name servers.
    
    This is useful for recursive resolution.

---
+++ src/NamedRoot.php
@@ -0,0 +1,92 @@
+<?php
+
+
+declare( strict_types = 1 );
+
+
+namespace JDWX\DNSQuery;
+
+
+use JDWX\DNSQuery\RR\A;
+use JDWX\DNSQuery\RR\AAAA;
+use JDWX\DNSQuery\RR\NS;
+use JDWX\DNSQuery\RR\RR;
+
+
+/** This class allows querying a named.root file for IP information about root name servers. */
+class NamedRoot {
+
+
+    /** The name of the named.root file. */
+    public const DEFAULT_NAMED_ROOT_FILE = __DIR__ . '/../data/named.root';
+
+
+    protected array $records = [];
+
+
+    /** Construct the object, optionally using a custom file.
+     * @throws Exception
+     */
+    public function __construct( string $i_namedRootPath = null ) {
+        if ( is_null( $i_namedRootPath ) ) {
+            $i_namedRootPath = self::DEFAULT_NAMED_ROOT_FILE;
+        }
+        foreach ( file( $i_namedRootPath ) as $line ) {
+            $line = strtolower( trim( $line ) );
+            if ( "" === $line ) {
+                continue;
+            }
+            if ( ";" === $line[0] ) {
+                continue;
+            }
+            $rr = RR::fromString( $line );
+            if ( ! array_key_exists( $rr->name, $this->records ) ) {
+                $this->records[ $rr->name ] = [];
+            }
+            if ( ! array_key_exists( $rr->type, $this->records[ $rr->name ] ) ) {
+                $this->records[ $rr->name ][ $rr->type ] = [];
+            }
+            $this->records[ $rr->name ][ $rr->type ][] = $rr;
+        }
+    }
+
+
+    /** Get a list of the root name server IP addresses. */
+    public function listAddresses( bool $i_useIPv4 = true, bool $i_useIPv6 = false ) : array {
+        $out = [];
+        foreach ( $this->listNameServers() as $nameServer ) {
+            if ( $i_useIPv4 ) {
+                foreach ( $this->records[ $nameServer ][ 'A' ] as $rr ) {
+                    assert( $rr instanceof A );
+                    $out[] = $rr->address;
+                }
+            }
+            if ( $i_useIPv6 ) {
+                foreach ( $this->records[ $nameServer ][ 'AAAA' ] as $rr ) {
+                    assert( $rr instanceof AAAA );
+                    $out[] = $rr->address;
+                }
+            }
+        }
+        return $out;
+    }
+
+
+    /** Get a list of the root name server names.
+     *
+     * This should return a.root-servers.net through m.root-servers.net unless
+     * the Internet changes quite dramatically.
+     *
+     * @return string[] The list of name servers.
+     */
+    public function listNameServers() : array {
+        $out = [];
+        foreach ( $this->records[ '' ][ 'NS' ] as $record ) {
+            assert( $record instanceof NS );
+            $out[] = $record->nsdName;
+        }
+        return $out;
+    }
+
+
+}
---
+++ tests/NamedRootTest.php
@@ -0,0 +1,98 @@
+<?php
+
+
+declare( strict_types = 1 );
+
+
+namespace JDWX\DNSQuery\tests;
+
+
+use JDWX\DNSQuery\NamedRoot;
+use PHPUnit\Framework\TestCase;
+
+
+/** Test the NamedRoot class. */
+class NamedRootTest extends TestCase {
+
+
+    /** @var array|string[] The list of root name servers. */
+    public static array $rootNameServers = [
+        'a.root-servers.net',
+        'b.root-servers.net',
+        'c.root-servers.net',
+        'd.root-servers.net',
+        'e.root-servers.net',
+        'f.root-servers.net',
+        'g.root-servers.net',
+        'h.root-servers.net',
+        'i.root-servers.net',
+        'j.root-servers.net',
+        'k.root-servers.net',
+        'l.root-servers.net',
+        'm.root-servers.net',
+    ];
+
+
+    /** @var array|string[] A list of root name server IPv4 addresses sorted by IP. */
+    public static array $rootNameServersIPv4 = [
+        '192.112.36.4',   # g.root-servers.net
+        '192.203.230.10', # e.root-servers.net
+        '192.33.4.12',    # c.root-servers.net
+        '192.36.148.17',  # i.root-servers.net
+        '192.5.5.241',    # f.root-servers.net
+        '192.58.128.30',  # j.root-servers.net
+        '193.0.14.129',   # k.root-servers.net
+        '198.41.0.4',     # a.root-servers.net
+        '198.97.190.53',  # h.root-servers.net
+        '199.7.83.42',    # l.root-servers.net
+        '199.7.91.13',    # d.root-servers.net
+        '199.9.14.201',   # b.root-servers.net
+        '202.12.27.33',   # m.root-servers.net
+    ];
+
+
+    /** Test the list of root name server names. */
+    public function testRootNames() {
+        $root = new NamedRoot();
+        $check = $root->listNameServers();
+        sort( $check );
+        static::assertSame( static::$rootNameServers, $check );
+    }
+
+
+    /** Check the list of returned IPv4 addresses for the root name servers. */
+    public function testNamedRootIPv4() {
+        $namedRoot = new NamedRoot();
+        $check = $namedRoot->listAddresses();
+        sort( $check );
+        static::assertSame( static::$rootNameServersIPv4, $check );
+    }
+
+
+    /** Test the IPv6 root name server addresses. */
+    public function testNamedRootIPv6() {
+        $rootNameServersIPv6 = [
+            '2001:503:ba3e::2:30',  # a.root-servers.net
+            '2001:500:200::b',      # b.root-servers.net
+            '2001:500:2::c',        # c.root-servers.net
+            '2001:500:2d::d',       # d.root-servers.net
+            '2001:500:a8::e',       # e.root-servers.net
+            '2001:500:2f::f',       # f.root-servers.net
+            '2001:500:12::d0d',     # g.root-servers.net
+            '2001:500:1::53',       # h.root-servers.net
+            '2001:7fe::53',         # i.root-servers.net
+            '2001:503:c27::2:30',   # j.root-servers.net
+            '2001:7fd::1',          # k.root-servers.net
+            '2001:500:9f::42',      # l.root-servers.net
+            '2001:dc3::35',         # m.root-servers.net
+        ];
+        sort( $rootNameServersIPv6 );
+
+        $namedRoot = new NamedRoot();
+        $check = $namedRoot->listAddresses( false, true );
+        sort( $check );
+        static::assertSame( $rootNameServersIPv6, $check );
+    }
+
+
+}
