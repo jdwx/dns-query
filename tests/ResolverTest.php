03cce0ad77f4ab637446fcc6c5c4b6a8a125b045
JDW <xs@jdw.sx>
2025-06-18 15:39:49 -0400

    Update domain used for testing.
    Google's A records change too fast.

--- tests/ResolverTest.php
+++ tests/ResolverTest.php
@@ -91,8 +91,8 @@ final class ResolverTest extends TestCase {
         $rActual = Resolver::dns_get_record( 'google.com', DNS_MX );
         $this->compareRRArrays( $rExpected, $rActual );
 
-        $rExpected = dns_get_record( 'google.com', DNS_A );
-        $rActual = Resolver::dns_get_record( 'google.com', DNS_A );
+        $rExpected = dns_get_record( 'iana.org', DNS_A );
+        $rActual = Resolver::dns_get_record( 'iana.org', DNS_A );
         $this->compareRRArrays( $rExpected, $rActual );
 
         $rExpected = dns_get_record( 'www.amazon.com', DNS_CNAME );
